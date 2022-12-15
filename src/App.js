07b972f9510fa5353417b676cebbd7d8fb901dca
Magno Gomes da Silva <114622958+magnogsilva@users.noreply.github.com>
2022-12-15 01:28:38 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import Frase from './components/Frase';
+import SeuNome from './components/SeuNome';
+import { useState } from 'react';
+
+function App() {
+
+  const name = 'magno gomes'
+  const newName = name.toUpperCase()
+  const [number, setNumber] = useState(0);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{newName}</p>
+        <h3>
+          Componente Principal
+        </h3>
+        <Frase/>
+        <SeuNome 
+        nome='Magno'
+        idade='37'
+        profissao='Estudante Softex'
+        />
+        <div>
+          <p>Você clicou {number} vezes.</p>
+          <button onClick={() => setNumber(number + 1)}>
+            Click aqui
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
